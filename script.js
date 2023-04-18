848bbded70216b095d651a1735a85e4fd91273d0
Diego Cámere Reynoso <dcamere@gmail.com>
2023-04-17 19:40:02 -0500

    Primera sesión

---
+++ script.js
@@ -0,0 +1,14 @@
+const name = "Jorge";
+let age = 30;
+let todoList = ["Comprar", "Lavar", "Cocinar", "Ejercitar"];
+
+
+console.log(name + " tiene " + age + " años");
+console.log(todoList);
+
+
+
+
+const img = document.querySelector(".dog");
+console.log(img);
+img.style.border = "5px red solid"
