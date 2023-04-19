09b079ba3a993dd08ba2deac4548a606fca4ba5f
Diego Cámere Reynoso <dcamere@gmail.com>
2023-04-18 19:53:44 -0500

    JS

--- script.js
+++ script.js
@@ -1,14 +1,81 @@
-const name = "Jorge";
-let age = 30;
-let todoList = ["Comprar", "Lavar", "Cocinar", "Ejercitar"];
+// const age1 = 21;
+// const age2 = 10;
+
+// if (age2 > 18) {
+//     console.log("Es mayor de edad!!");
+// } else {
+//     console.log("Es menor de edad!!");
+// }
+
+// const city = "Moquegua";
+
+// switch (city) {
+//     case "Lima": 
+//         console.log("City: Lima"); 
+//         console.log("Hay envío"); 
+//         break;
+//     case "Arequipa": 
+//         console.log("City: Lima"); 
+//         console.log("Hay envío limitado por distritos"); 
+//         break;
+//     case "Cusco": 
+//         console.log("City: Lima"); 
+//         console.log("No hay envío"); 
+//         break;
+//     default: console.log("Por defecto se ejecuta esta línea")
+// }
+
+// const list = ["Lavar", "Cocinar", "Tender cama"];
+
+// for (let i = 0; i < list.length; i++) {
+//     console.log(list[i]);
+// }
+
+// console.log(list[list.length - 1])
 
 
-console.log(name + " tiene " + age + " años");
-console.log(todoList);
+const myFirstFunction = function () {
+    console.log("Esta es mi primera función");
+}
+
+// myFirstFunction();
+
+// console.log(myFirstFunction());
 
 
+const dividir = function (n1, n2) {
+    return n1 / n2;
+}
+
+// console.log(dividir(10, 15));
 
 
-const img = document.querySelector(".dog");
-console.log(img);
-img.style.border = "5px red solid"
+const button = document.querySelector("#button");
+const input = document.querySelector("#input");
+console.log(input);
+
+const onClickFunction = function () {
+    console.log("El botón ha sido clickeado!")
+}
+
+button.addEventListener("click", function (e) {
+    console.log(e);
+});
+
+input.addEventListener("keypress", function (e) {
+    console.log(e.key)
+})
+
+window.addEventListener("resize", function () {
+    console.log(window.innerWidth)
+    if (this.window.innerWidth > 768) {
+        console.log("Estamos en una pc/laptop")
+    } else {
+        console.log("Estamos en un dispositivo móvil")
+    }
+})
+
+window.addEventListener("scroll", function () {
+    console.log("hola")
+})
+
